3be38343806750269cf0fd9ee6e70d1cb76802de
Roman Rudalev <rudalev310305@gmail.com>
2026-04-01 20:21:33 +0300

    Added typeErrors

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,22 @@
+use std::collections::HashMap;
+
+use crate::term::Term;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Type {
     Bool,
     Nat,
     Func(Box<Type>, Box<Type>),
 }
+
+pub type Context = HashMap<String, Type>;
+
+#[derive(Debug, Clone)]
+pub enum TypeError {
+    UnboundVariable(String),
+    Mismatch(Type, Type),
+    ExpectedBool(Type),
+    ExpectedNat(Type),
+    ExpectedFunction(Type),
+}
+
